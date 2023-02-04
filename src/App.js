11184941e56edfd160d2fd3769a9a07297595d3f
hayden <hayden.ismet@amigotechnology.com>
2023-02-04 02:02:55 +0000

    styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import placeholder from "./assets/img/placeholder.png";
 import logo from "./assets/img/Logo.svg";
 import add from "./assets/img/Add.svg";
 import want from "./assets/img/Want.svg";
+import github from "./assets/img/github-mark-white.svg";
 
 function App() {
   return (
@@ -35,12 +36,469 @@ function App() {
             <li>Rebel Clash</li>
   <li>Sword & Shield</li>*/}
           </ul>
-          <footer>hayden ismet</footer>
+          <footer>
+            <img src={github} alt="Github" />
+          </footer>
         </nav>
         <section className="results">
-          <h1>
-            Sword & Shield<h6>Crown Zenith</h6>
-          </h1>
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="placeholder">
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className="placeholder-image"
+            />
+            <div className="details-interaction">
+              <div className="details">
+                <div className="name">Deoxys</div>
+                <div className="number">GG12/GG70</div>
+                <div className="base">Sword & Shield</div>
+                <div className="sub">Crown Zenith</div>
+              </div>
+              <div className="interaction">
+                <div className="icons">
+                  <div className="icon-want">
+                    <img src={want} alt="want this card" />
+                  </div>
+                  <div className="icon-have">
+                    <img src={add} alt="add this card" />
+                  </div>
+                </div>
+                <div className="release">2023</div>
+              </div>
+            </div>
+          </div>
 
           <div className="placeholder">
             <img
